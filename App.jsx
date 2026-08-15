c181f015d28500c0be7a2728578f2b0db0603567
curnio <dipeshkumarpatel215@gmail.com>
2026-08-15 17:05:31 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -9,8 +9,8 @@ export default function App() {
   return (
     <div style={{ background: '#020408', minHeight: '100vh' }}>
       <Nav />
-      <Hero />
-      <Track />
+      <Hero /> 
+      <Track /> /*I had to remove the api function atleast becuase the api was not working with deloyed version no matter what I did.*/
       <About />
       <Credit />
     </div>
